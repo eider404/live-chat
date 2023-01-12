5b32934ae40f186b5854df02a706e7fb08cbbf13
Eider Pool <eider.pool.arjona@gmail.com>
2023-01-11 20:44:12 -0600

    frondend - sing-in realizado

--- Frondend/index.js
+++ Frondend/index.js
@@ -18,3 +18,27 @@ function SingUp(){
             })
             .catch(err => console.log(err));
 }
+
+//Sing In
+function SingIn(){
+    let formSingIn = document.querySelector( '#form-sing-in' )
+
+    const obj = {}
+    new FormData( formSingIn ).forEach( ( value, key ) => obj[ key ] = value )
+
+    fetch( `http://localhost:3000/sign-in/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify( obj )
+            })
+            .then(res => res.json())
+            .then( data =>{
+                document.querySelector("#respuesta").innerHTML= JSON.stringify(data.mensaje) 
+                //syntax
+                localStorage.setItem('token', data.token)
+                //generarMsg(data);
+            })
+            .catch(err => console.log(err));
+}
